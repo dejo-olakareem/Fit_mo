ea1b8cf39f58a505c5698fb1da5337556a1d3af0
dejo-olakareem <oladejoolakareem@yahoo.com>
2017-01-05 08:54:49 -0800

    Add Edit page and also edit route edit

--- app/controllers/user_contoller.rb
+++ app/controllers/user_contoller.rb
@@ -48,7 +48,7 @@ get '/users/:id/edit' do
   #get params from url
   @user = User.find(params[:id]) #define intstance variable for view
 
-  erb :'users/edit' #show edit user view
+  erb :"users/edit.html" #show edit user view
 
 end
 
@@ -61,9 +61,9 @@ put '/users/:id' do
   @user.assign_attributes(params[:user]) #assign new attributes
 
   if @user.save #saves new user or returns false if unsuccessful
-    redirect '/users' #redirect back to users index page
+    redirect '/users/#{user.id}' #redirect back to users index page
   else
-    erb :'users/edit' #show edit user view again(potentially displaying errors)
+    erb :"users/edit.html" #show edit user view again(potentially displaying errors)
   end
 
 end
