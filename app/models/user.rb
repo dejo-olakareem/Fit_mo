fd4527835129296bbcbfd82edb68359c0dcb151f
dejo-olakareem <oladejoolakareem@yahoo.com>
2017-01-04 20:37:54 -0800

    Insert Bcrypt method into user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,27 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
   has_many :images
+
+  # users.password_hash in the database is a :string
+  include BCrypt
+
+  def password
+  	@password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+  	@password = Password.create(new_password)
+  	self.password_hash = @password
+  end
+
+  # def login
+  # 	@user = User.find_by_email(params[:email])
+  # 	if @user.password == params[:password]
+  # 		give_token
+  # 	else
+  # 		redirect_to home_url
+  # 	end
+  # end
+
+
 end
